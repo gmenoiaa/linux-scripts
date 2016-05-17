328551d396e2cb1c396c48d65c7c771803ebb2ab
Geiser Menoia <gmenoiaa@gmail.com>
2016-05-17 13:58:28 -0300

    add 90fixanyconnectmtu.sh

---
+++ 90fixanyconnectmtu.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# this should be symlinked to /etc/NetworkManager/dispatcher.d/
+# make sure this file is:
+# - owned by root (chown root:root <filename>)
+# - executable (chmod +x <filename>)
+# you can check log at /var/log/syslog
+
+INTERFACE=$1
+STATUS=$2
+
+if [ "$INTERFACE" == "tun0" ]
+then
+    case "$STATUS" in
+        up)
+	logger -s "vpn kenzan up triggered"
+	sudo ifconfig tun0 mtu 1356
+        ;;
+        *)
+        ;;
+    esac
+fi
